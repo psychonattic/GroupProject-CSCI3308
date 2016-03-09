8751721976183e673cd08922f3d93d483bf4d9c1
Joel Barkley <joel.barkley@colorado.edu>
2016-03-09 12:38:18 -0700

    fixed gameboard so it still runs

--- mememonopoly.py
+++ mememonopoly.py
@@ -19,51 +19,51 @@ from gameboard import *
 
 
 def main():
-	#board = new GameBoard()
+	board = GameBoard(700,20)
 	#boardImage = pygame.image.load('monopoly.png')
 	#boardgamebg = pygame.image.load('monopoly.png')
-	startScreen()
+	board.startScreen()
 	while True:
-		if run() == False:
+		if board.run() == False:
 			break
-		fpsClock.tick(Framepersecond) #limits how fast the screen can updat by framepersecond
+		board.fpsClock.tick(board.Framepersecond) #limits how fast the screen can updat by framepersecond
 
 
-def startScreen():
-	global DISPLAY, GAMEFONT, fpsClock
-	pygame.init() #initializes the board
-	fpsClock = pygame.time.Clock() #keeps track of how fast the screen is updating
-	GAMEFONT = pygame.font.Font('freesansbold.ttf', 32)
-	DISPLAY = pygame.display.set_mode((boardsize, boardsize))
-	DISPLAY.fill(BLACK)
-	pygame.display.set_caption('Mod Monopoly') 
-	STARTFONT = pygame.font.Font('freesansbold.ttf', 25)  #font for start screen
-	newGameStart = STARTFONT.render('Start', True, TEXTCOLOR, BGCOLOR) #renders start button
-	newGameRect = newGameStart.get_rect() #gets the rect value of start button
-	newGameRect.center = (int(boardsize / 4), (boardsize-(newGameRect.height))) #places start button on botton of the screen
-	newGameExit = STARTFONT.render('Exit', True, TEXTCOLOR, BGCOLOR) #renders exit button
-	newGameExitRect = newGameExit.get_rect() #gets the exit button rect value
-	newGameExitRect.center = (int(3*(boardsize / 4)), (boardsize-(newGameRect.height))) #puts exit button on bottom of the screen
-	text1 = "Welcome to Mod-opoly!" #welcome message
-	text2 = "Click 'Start' to continue or 'Exit' to quit" #instructions message
-	screen_text1 = STARTFONT.render(text1, True, WHITE) #renders welcome message
-	screen_text2 = STARTFONT.render(text2, True, WHITE) #render instruction message
-	while True:
-		DISPLAY.blit(screen_text1, [boardsize/4,boardsize/4]) #displays welcome message
-		DISPLAY.blit(screen_text2, [boardsize/8,boardsize/2]) #displays instruction message
-		DISPLAY.blit(newGameStart,newGameRect) #display start button
-		DISPLAY.blit(newGameExit,newGameExitRect) #display exit button
-		pygame.display.update() #updates the screen
-		for event in pygame.event.get():
-			if event.type == MOUSEBUTTONUP:  #checks for mouse click
-				mousex, mousey = event.pos #gets coordinates of mouse click
-				if newGameRect.collidepoint((mousex, mousey)): #checks if mouse click is in start button
-					print "collided"
-					DISPLAY.fill(BLACK) #resets the screen
-					return False #exits start screen
-				elif newGameExitRect.collidepoint((mousex,mousey)): #checks if mouse click is in exit button
-					print "exit"
-					sys.exit() #exits the game
+# def startScreen():
+# 	global DISPLAY, GAMEFONT, fpsClock
+# 	pygame.init() #initializes the board
+# 	fpsClock = pygame.time.Clock() #keeps track of how fast the screen is updating
+# 	GAMEFONT = pygame.font.Font('freesansbold.ttf', 32)
+# 	DISPLAY = pygame.display.set_mode((boardsize, boardsize))
+# 	DISPLAY.fill(BLACK)
+# 	pygame.display.set_caption('Mod Monopoly') 
+# 	STARTFONT = pygame.font.Font('freesansbold.ttf', 25)  #font for start screen
+# 	newGameStart = STARTFONT.render('Start', True, TEXTCOLOR, BGCOLOR) #renders start button
+# 	newGameRect = newGameStart.get_rect() #gets the rect value of start button
+# 	newGameRect.center = (int(boardsize / 4), (boardsize-(newGameRect.height))) #places start button on botton of the screen
+# 	newGameExit = STARTFONT.render('Exit', True, TEXTCOLOR, BGCOLOR) #renders exit button
+# 	newGameExitRect = newGameExit.get_rect() #gets the exit button rect value
+# 	newGameExitRect.center = (int(3*(boardsize / 4)), (boardsize-(newGameRect.height))) #puts exit button on bottom of the screen
+# 	text1 = "Welcome to Mod-opoly!" #welcome message
+# 	text2 = "Click 'Start' to continue or 'Exit' to quit" #instructions message
+# 	screen_text1 = STARTFONT.render(text1, True, WHITE) #renders welcome message
+# 	screen_text2 = STARTFONT.render(text2, True, WHITE) #render instruction message
+# 	while True:
+# 		DISPLAY.blit(screen_text1, [boardsize/4,boardsize/4]) #displays welcome message
+# 		DISPLAY.blit(screen_text2, [boardsize/8,boardsize/2]) #displays instruction message
+# 		DISPLAY.blit(newGameStart,newGameRect) #display start button
+# 		DISPLAY.blit(newGameExit,newGameExitRect) #display exit button
+# 		pygame.display.update() #updates the screen
+# 		for event in pygame.event.get():
+# 			if event.type == MOUSEBUTTONUP:  #checks for mouse click
+# 				mousex, mousey = event.pos #gets coordinates of mouse click
+# 				if newGameRect.collidepoint((mousex, mousey)): #checks if mouse click is in start button
+# 					print "collided"
+# 					DISPLAY.fill(BLACK) #resets the screen
+# 					return False #exits start screen
+# 				elif newGameExitRect.collidepoint((mousex,mousey)): #checks if mouse click is in exit button
+# 					print "exit"
+# 					sys.exit() #exits the game
 
 			
 
